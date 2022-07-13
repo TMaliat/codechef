f82ea3129b992d5e8749272ff8df54ea5d2d8424
Tanzeem <95355941+TMaliat@users.noreply.github.com>
2022-07-14 01:08:29 +0600

    Update HEIGHTS.cpp

--- HEIGHTS.cpp
+++ HEIGHTS.cpp
@@ -1,43 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-int frequency_controller(int *array,int size){
-    int freq[10000];
-    int count;
-    for(int i=0; i<size; i++) freq[i] = -1;
-    for(int i=0; i<size; i++)
-    {
-        count = 1;
-        for(int j=i+1; j<size; j++)
-        {
-            if(array[i]==array[j])
-            {
-                count++;
-                freq[j] = 0;
-            }
-        }
-        if(freq[i] != 0)
-        {
-            freq[i] = count;
-        }
-    }
-    int frequency=0;
-    for(int i=0;i<size;i++){
-        if(freq[i]==1) frequency++;
-    }
-    return frequency;
-}
 int main(){
     int test; cin >> test;
     while(test--){
         int size; cin >> size;
-        int array[10000];
-        for(int i=0;i<size;i++) cin >> array[i];
-        int res=frequency_controller(array,size);
-        if(res==0) cout << "0" << endl;
-        else if(res==1) cout << "1" << endl;
-        else if(res>1) {
-            if(res%2==0) cout << res/2 << endl;
-            else cout << (res/2)+1 << endl;
+        vector<int>v(size+1);
+        map<int,int>mp;
+        for(int i=0;i<size;i++){
+            cin >> v[i];
+            int x=v[i];
+            mp[x]++;
+        }
+        int count=0,maxFreq=0;
+        for(auto it:mp){
+            int x=it.first;
+            int freq=it.second;
+            maxFreq=max(maxFreq,freq);
+            if(freq==1) count++;
+        }
+        if(count!=1) //count==0 or count>=2
+            cout << (count+1)/2 << endl;
+        else{
+            sort(v.begin()+1,v.end());
+            if(mp[v[size]]!=1) cout << "1" << endl;
+            else{
+                if(maxFreq>2) cout << "1" << endl;
+                else cout << "2" << endl;
+            }
         }
     }
 }
